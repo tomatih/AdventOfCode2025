3607ca0c48801497ad4df23fe6c739ae31677ee5
to_matih <maatteeoh@wp.pl>
2025-12-01 08:20:52 +0000

    Day 1 part 1

--- src/Day1/part1.rs
+++ src/Day1/part1.rs
@@ -1,3 +1,38 @@
-fn main() {
-	println!("d1p1 test!");
+use std::io;
+
+use anyhow::Result;
+
+const DIAL_SIZE: i32 = 100;
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+
+    let mut dial_pos: i32 = 50;
+    let mut output = 0;
+
+    for line in stdin.lines() {
+        let line = line?;
+
+        let direction = line.chars().nth(0).unwrap();
+        let amount = &line[1..].parse::<i32>()?;
+
+        match direction {
+            'R' => {
+                dial_pos += amount;
+            }
+            'L' => {
+                dial_pos -= amount;
+            }
+            _ => panic!("Unknown direction indicator"),
+        }
+
+        dial_pos = dial_pos.rem_euclid(DIAL_SIZE);
+
+        if dial_pos == 0 {
+            output += 1;
+        }
+    }
+
+    println!("Password: {}", output);
+    Ok(())
 }
