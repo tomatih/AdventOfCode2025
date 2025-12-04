6cedf1d999a3dca1f279d98f5331db5ef3f0dfc7
to_matih <maatteeoh@wp.pl>
2025-12-04 08:29:03 +0000

    Day 4 part 1

---
+++ src/Day4/part1.rs
@@ -0,0 +1,51 @@
+use anyhow::Result;
+use std::io::stdin;
+
+fn main() -> Result<()> {
+    let mut out = 0u64;
+
+    let mut world_map: Vec<Vec<u8>> = Vec::new();
+    for line in stdin().lines() {
+        let line = line?;
+        world_map.push(
+            line.chars()
+                .map(|c| match c {
+                    '.' => 0u8,
+                    '@' => 1u8,
+                    _ => panic!("Unknown character"),
+                })
+                .collect(),
+        );
+    }
+
+    for y in 0..world_map.len() {
+        for x in 0..world_map[y].len() {
+            let mut accum = 0u8;
+
+            if world_map[y][x] == 0 {
+                continue;
+            }
+
+            for dy in -1isize..=1 {
+                for dx in -1isize..=1 {
+                    if dx == 0 && dy == 0 {
+                        continue;
+                    }
+
+                    if let Some(row) = world_map.get((y as isize + dy) as usize) {
+                        if let Some(val) = row.get((x as isize + dx) as usize) {
+                            accum += val;
+                        }
+                    }
+                }
+            }
+
+            if accum < 4 {
+                out += 1;
+            }
+        }
+    }
+
+    println!("Accesible rolls: {}", out);
+    Ok(())
+}
