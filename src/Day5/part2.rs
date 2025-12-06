7c339b621a6e67c058129478a75dc672a1b948c0
to_matih <maatteeoh@wp.pl>
2025-12-06 10:21:01 +0000

    Day 5 part 2

--- src/Day5/part2.rs
+++ src/Day5/part2.rs
@@ -3,7 +3,6 @@ use std::{io::stdin, ops::RangeInclusive};
 
 fn main() -> Result<()> {
     let mut ranges: Vec<RangeInclusive<u64>> = Vec::new();
-    let mut out = 0u64;
 
     for line in stdin().lines() {
         let line = line?;
@@ -18,78 +17,28 @@ fn main() -> Result<()> {
         let upper_bound = bounds.next().unwrap().parse::<u64>()?;
 
         let range = lower_bound..=upper_bound;
-        let mut needs_adding = Some(ranges.len());
 
-        for i in 0..ranges.len() {
-            // range already fully accoubnted for
-            if ranges[i].contains(&lower_bound) && ranges[i].contains(&upper_bound) {
-                needs_adding = None;
-                println!("Dropping {:?}", range);
-                break;
-            }
-            // can extend a ranhe to the left
-            else if ranges[i].contains(&upper_bound) {
-                // check if can be merged with the previous range
-                if i != 0 && ranges[i - 1].contains(&lower_bound) {
-                    println!(
-                        "Used {:?} to join {:?} and {:?}",
-                        range,
-                        ranges[i - 1],
-                        ranges[i]
-                    );
-                    ranges[i - 1] = *ranges[i - 1].start()..=*ranges[i].end();
-                    ranges.remove(i);
-                } else {
-                    ranges[i] = lower_bound..=*ranges[i].end();
-                    println!("Merged {:?} with {:?}", range, ranges[i]);
-                }
-                needs_adding = None;
-                break;
-            }
-            // can extend to the right
-            else if ranges[i].contains(&lower_bound) {
-                // check if can be merged with the previous range
-                if i + 1 < ranges.len() && ranges[i + 1].contains(&upper_bound) {
-                    println!(
-                        "Used {:?} to join {:?} and {:?}",
-                        range,
-                        ranges[i],
-                        ranges[i + 1]
-                    );
-                    ranges[i] = *ranges[i].start()..=*ranges[i + 1].end();
-                    ranges.remove(i + 1);
-                } else {
-                    ranges[i] = lower_bound..=*ranges[i].end();
-                    println!("Merged {:?} with {:?}", range, ranges[i]);
-                }
-                needs_adding = None;
-                break;
-            }
-            // foudn insertion place for the range
-            else if ranges[i].end() < &lower_bound
-                && i + 1 < ranges.len()
-                && ranges[i + 1].start() > &upper_bound
-            {
-                println!("Found an insersion spot {}", i);
-                needs_adding = Some(i + 1);
-                break;
-            }
-        }
+        ranges.push(range);
+    }
 
-        if let Some(i) = needs_adding {
-            println!("Adding {:?} at pos {}", range, i);
-            ranges.insert(i, range);
+    ranges.sort_by_key(|r| *r.start());
+
+    let mut i = 0;
+    while i < ranges.len() {
+        // ranges are mergable
+        if i + 1 < ranges.len() && ranges[i + 1].start() <= ranges[i].end() {
+            // next range not already fully accounted for
+            if !(ranges[i + 1].end() <= ranges[i].end()) {
+                ranges[i] = *ranges[i].start()..=*ranges[i + 1].end(); // merge with next one
+            }
+            ranges.remove(i + 1);
+        } else {
+            i += 1;
         }
     }
 
-    for range in ranges {
-        println!("{:?}", range);
-        out += range.end() - range.start() + 1;
-    }
-
+    let out: u64 = ranges.iter().map(|r| r.end() - r.start() + 1).sum();
     println!("Output: {}", out);
 
     Ok(())
 }
-
-// WWHAT IF NEW RANGE IS BIG ENOUGHT TO JOIN MULTIPLE RANGES!
